63faa79073b5e8355ca179a1f7bb2ff14d7db4bd
junhyuck park <wnsgur1855@naver.com>
2023-02-03 22:39:21 +0900

    부모자식 컴포넌트 구별

--- react/week-1/src/App.js
+++ react/week-1/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
+
+//자식 컴포넌트
+function Child() {
+  return <div>나는 자식이랑께</div>
+}
+
+//나는 부모입니다ㅣ
 function App() {
-  const onclickButtonHandler = () => {
-    alert('니똥');
-  }
- 
-  return (
-  /* <---- HTML/JSX 영역  ---->*/
-    <div
-      style={{
-        height: '100vh',
-        display: ' flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-        >
-      <span>이것은 내가 만든 App컴포넌트 입니당.</span>
-      <button onClick = {onclickButtonHandler}>클릭</button>
-        </div>
-      );
-    }
+  return <Child />  // = <div>나는 자식이랑께</div> 굳이 따로 분리함
+}
 
 export default App;
