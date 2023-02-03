00b768082942094b0c5c34845eb9655edd7ddbf7
junhyuck park <wnsgur1855@naver.com>
2023-02-03 23:45:15 +0900

    부모 컴퍼넌츠 실습(3개의 컴포넌트)

---
+++ react/week-1/src/Parantcomponent.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+
+
+function Son() {
+  return <div>나는 효자가 되고싶습니다</div>
+}
+
+
+function Mother() {
+  return <Son/>
+}
+
+
+function Grandfather() {
+  return <Mother/>
+}
+
+
+function App() {
+  return <Grandfather/>
+}
+
+export default App;
