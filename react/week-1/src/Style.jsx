35f78191112e3cd60cac1ccebad22b4a2826ca07
junhyuck park <wnsgur1855@naver.com>
2023-02-04 12:12:02 +0900

    react 스타일 꾸미기

---
+++ react/week-1/src/Style.jsx
@@ -0,0 +1,25 @@
+import React from 'react'       //"rfc" or "rfce" 
+
+
+
+function App() {
+  const number = 1;
+  const pTagStyle = {
+    color : "red",
+  }
+
+  return  <div>
+            <p style = {pTagStyle}>안녕하세요 리액트입니다</p>
+            {/* 주석을 사용하는 방법입니다 */}
+            {/* 삼항 연산자를 사용해볼게요 */}
+            <p style = {pTagStyle}>
+              {
+              number > 10 
+              ? number + "은 10보다 크다" 
+              : number + "은 10보다 작다"
+              }
+            </p>
+          </div>
+}
+
+export default App;
