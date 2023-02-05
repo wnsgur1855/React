8be544fbc59f330e44d0723421136fb4643c729b
junhyuck park <wnsgur1855@naver.com>
2023-02-06 02:20:18 +0900

    분히된 추가 컴포넌트

---
+++ style-project/src/components/Button.jsx
@@ -0,0 +1,8 @@
+
+
+    const Button = ({clickAddButtonHandler, children}) => {
+        return     <button onClick={clickAddButtonHandler}>{children}</button>
+      
+      }
+
+export default Button;
